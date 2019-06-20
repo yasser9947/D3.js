875512997089d3e410120ff3cdf996351135c4cc
Yasir S <yasser9946.1@gmail.com>
2019-06-20 13:47:28 +0300

    make it dynamic

---
+++ makeItdynamic/loopingWhithInttervals.js
@@ -0,0 +1,16 @@
+d3.interval(function(){
+//your cod here 
+
+},500)
+
+
+// create new loop 
+
+var myInterval = setInterval(function(){
+//your cod here 
+
+
+}, 500)
+
+//stop the loop
+clearInterval(myInterval)
